95f0a73d5b1dd74f8b520008abd0bdafcdb023a4
Kate Shlykova <fonarikkate@gmail.com>
2024-08-21 17:13:13 +0300

    feat: create function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+    let reversedN;
+
+    if(n < 0) {
+        reversedN = Number(String(n).split('-')[1].split('').reverse().join(''));
+    } else {
+        reversedN = Number(String(n).split('').reverse().join(''));
+    }
   
+  return reversedN;
 }
